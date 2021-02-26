6de54e4fab1276fb7a33310b7abee1b23241417f
James Leith <james@Jamess-MacBook-Pro-2.local>
2021-02-26 11:13:39 -0700

    updated to ng v8

--- src/app/calculation.service.ts
+++ src/app/calculation.service.ts
@@ -1,30 +1,39 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CalculationService {
+  apiUrl = 'http://localhost:5000/api/';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getAddtion(number1: number, number2: number) {
-    return this.http.get<number>('http://localhost:61377/addition/' + number1 + '/' + number2 + '/');
+    return this.http.get<number>(
+      this.apiUrl + 'add/' + number1 + '/' + number2
+    );
   }
 
   getSubtraction(number1: number, number2: number) {
-    return this.http.get<number>('http://localhost:61377/subtraction/' + number1 + '/' + number2 + '/');
+    return this.http.get<number>(
+      this.apiUrl + 'sub/' + number1 + '/' + number2
+    );
   }
 
   getDivision(number1: number, number2: number) {
-    return this.http.get<number>('http://localhost:61377/division/' + number1 + '/' + number2 + '/');
+    return this.http.get<number>(
+      this.apiUrl + 'div/' + number1 + '/' + number2
+    );
   }
 
   getMultiplication(number1: number, number2: number) {
-    return this.http.get<number>('http://localhost:61377/multiplication/' + number1 + '/' + number2 + '/');
+    return this.http.get<number>(
+      this.apiUrl + 'multiply/' + number1 + '/' + number2
+    );
   }
 
   getPercentage(number1: number) {
-    return this.http.get<number>('http://localhost:61377/percentage/' + number1 + '/');
+    return this.http.get<number>(this.apiUrl + 'percent/' + number1);
   }
 }
